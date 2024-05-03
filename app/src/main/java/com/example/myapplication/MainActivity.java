ab35497ae1ccd1eb7dbd80cf5a23dfbf1c02e4f1
arx64 <eror404eror@hackermail.com>
2024-05-03 20:39:42 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String[] arrName = {"Samsung","Sony","Apple","LG","Motorola",
+                "HTC","Acer","Lenovo","Oppo","Nokia","BlackBerry","Huawei","ZTE","Meizu","HP","Asus","Panasonic"};
+                ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, arrName);
+        ListView listView = (ListView) findViewById(R.id.id_list);
+        listView.setAdapter(adapter);
+}
+    }
